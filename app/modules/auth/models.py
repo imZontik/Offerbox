762d5ac853f8c3e1a8bab416736442a89fd4435d
imZontik <elvin-nadirov-321@mail.ru>
2025-12-06 13:19:03 +0300

    Add user module and user model

---
+++ app/modules/auth/models.py
@@ -0,0 +1,13 @@
+from app.infrastructure.database.base import Base
+
+from sqlalchemy import Column, UUID, String
+
+import uuid
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
